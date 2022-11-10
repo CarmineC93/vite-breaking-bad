1107bc91aa78658bbbd935c8326d2339e42b83f4
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-11-10 18:29:10 +0100

    add better style to grid

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -9,24 +9,41 @@ export default {
 
 <template>
     <div class="character-card text-center">
-        <img :src="character.img" :alt="character.name">
-        <h4>{{character.name}}</h4>
-        <h5> {{character.category}}</h5>
-        <h5> {{character.status}}</h5>
+        <div class="pic">
+            <img :src="character.img" :alt="character.name">
+        </div>
+        <div class="text">
+            <h5>{{character.name}}</h5>
+            <h6> {{character.category}}</h6>
+            <h6> {{character.status}}</h6>
+        </div>
+
     </div>
 </template>
 
 <style lang="scss" scoped>
     .character-card{
         background-color:  rgb(26, 26, 33);
-    
-        h4{
-            color: white;
-            text-transform: uppercase;
+        
+        
+        .pic{
+            padding: 1rem;
+            img{
+                max-height: 200px;
+            }
         }
 
-        h5{
-            color: gray;
+        .text{
+            padding-bottom:  1rem;
+
+            h5{
+                color: white;
+                text-transform: uppercase;
+            }
+
+            h6{
+                color: gray;
+            }
         }
     }
 
--- src/components/AppGrid.vue
+++ src/components/AppGrid.vue
@@ -17,8 +17,8 @@ export default {
 
 <template>
     <div class="grid">
-        <div class="row">
-            <div class="col-2" v-for="character in store.characters" :key="character.char_id">
+        <div class="row m-2 g-3">
+            <div class="col-5 col-md-3 col-sm-5" v-for="character in store.characters" :key="character.char_id">
                 <AppCard :character="character" />
             </div>
         </div>
